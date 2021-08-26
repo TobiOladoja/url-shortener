f92fa8c17044bd69186ff330ec40fef73bf2141b
TobiOladoja <tbfullstack@gmail.com>
2021-08-26 16:59:26 +0100

    Static display with material ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+import { Grid } from '@material-ui/core'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid className="App" container direction="column" justify="center" alignItems="center">
+       <h1>Link Generator</h1>
+    </Grid>
   );
 }
 
